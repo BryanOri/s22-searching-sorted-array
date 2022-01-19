e617218ea9381b6c980981e7cffa221787703983
Jonathan C <jsea@uw.edu>
2022-01-19 15:44:55 -0800

    Assignment for creating a search algorithm based off searching a dictionary in real life

---
+++ searching.js
@@ -0,0 +1,20 @@
+let numbers = [ 3, 1, 9, 20, 41, 8, 56, 17, 2, 13, 26, 30, 4, 49 ];
+
+// STEP 1: Sort the "numbers" array from smallest number to largest
+//         number and ensure the "numbers" array is sorted afterwards
+//         in the console
+
+
+
+// STEP 2: Write an algorithm that searches through the sorted array
+//         for a supplied number, as if it was a word in a dictionary.
+
+
+
+// STEP 3: Ensure the algorithm gives expected results for both for
+//         numbers that are in the array and numbers that are not in
+//         the array
+//
+//         Example: 100 should not be found, but 49 should be found.
+
+
