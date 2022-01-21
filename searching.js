1c826a38bfaab53dc812402b1df7703032ea651f
Jonathan C <jsea@uw.edu>
2022-01-21 12:23:28 -0800

    Updated the instructions to be clearer to understand.

--- searching.js
+++ searching.js
@@ -7,13 +7,24 @@ let numbers = [ 20, 9, 3, 41, 8, 1, 56 ];
 
 
 // STEP 2: Write an algorithm that searches through the sorted array
-//         for a supplied number, as if it was a word in a dictionary.
+//         for a supplied number, as if it were a word in a dictionary.
+//
+//         Example: Check the value in the center of the unchecked
+//                  values. If it is the value you're looking for,
+//                  return that value. If it is greater than the value
+//                  you're looking for, forget the current value and
+//                  all values greater than the current value, use the
+//                  remaining set of unchecked values, and start over.
+//                  If there are no values remaining in the set of
+//                  unchecked values, consider the value you're
+//                  searching for to be not in the array.
 
 
 
 // STEP 3: Ensure the algorithm gives expected results for numbers
-//         that are in the array and numbers that are not in the array
+//         that are in the array and numbers that are not in the array.
 //
-//         Example: 100 should NOT be found, but 41 should be found.
+//         Example: 100 and 0 should NOT be found, but 41 and any
+//                  number in the "numbers" array should be found.
 
 
