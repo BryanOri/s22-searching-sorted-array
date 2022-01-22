33c43d461afff080afa8cb5e9ef4034ad093428d
BryanOri <bryanorihuela@computingforall.org>
2022-01-22 11:46:09 -0800

    stuff

--- searching.js
+++ searching.js
@@ -4,7 +4,22 @@ let numbers = [ 20, 9, 3, 41, 8, 1, 56 ];
 //         number and ensure the "numbers" array is sorted afterwards
 //         in the console
 
-
+function algorithms(_array2){
+    for (i = 0; i < _array2.length; i++){
+      let smallest = i    
+      for ( indx = i + 1; indx < _array2.length; indx++){
+        if( _array2[smallest] > _array2[indx]){
+          smallest = indx;
+        } 
+      }
+      let substitute = _array2[i]
+      _array2[i] = _array2[smallest];
+      _array2[smallest] = substitute;
+    }
+   return _array2;
+  } 
+algorithms(numbers);
+console.log(numbers)
 
 // STEP 2: Write an algorithm that searches through the sorted array
 //         for a supplied number, as if it were a word in a dictionary.
@@ -19,7 +34,17 @@ let numbers = [ 20, 9, 3, 41, 8, 1, 56 ];
 //                  unchecked values, consider the value you're
 //                  searching for to be not in the array.
 
-
+function search( _array, find){
+  let found = 0;
+    for (i of _array){
+        if (i === find){
+            found = i;
+            return _array.indexOf(found);
+        }else{
+          continue;
+        }
+    }
+}   
 
 // STEP 3: Ensure the algorithm gives expected results for numbers
 //         that are in the array and numbers that are not in the array.
@@ -27,4 +52,4 @@ let numbers = [ 20, 9, 3, 41, 8, 1, 56 ];
 //         Example: 100 and 0 should NOT be found, but 41 and any
 //                  number in the "numbers" array should be found.
 
-
+console.log(search(numbers, 41));
